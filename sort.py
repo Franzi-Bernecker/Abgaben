50eeecb0b373090d718bdd94a85bd10e010c0146
laurenzkeller05 <laurenz.keller05@gmail.com>
2026-05-13 12:52:00 +0200

    Add main script, implement power curve plotting, and update dependencies

--- sort.py
+++ sort.py
@@ -1,10 +1,11 @@
-def bubble_sort(arr):
-    n = len(arr)
+def bubble_sort(liste):
+    n = len(liste)
     for i in range(n):
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-    return arr
+        for j in range(n - 1):
+            if liste[j] > liste[j + 1]:
+                liste[j], liste[j + 1] = liste[j + 1], liste[j]
+    return liste
 
 
-
+if __name__ == "__main__":
+    print(bubble_sort([5, 3, 1, 4, 2]))
