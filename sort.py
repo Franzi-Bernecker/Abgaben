4c80ae70007cdc2acef8a057cf4a1361e1a23065
laurenzkeller05 <laurenz.keller05@gmail.com>
2026-05-13 23:29:29 +0200

    Refactor bubble_sort function in sort.py to remove unnecessary whitespace and ensure proper formatting.

--- sort.py
+++ sort.py
@@ -5,7 +5,3 @@ def bubble_sort(liste):
             if liste[j] > liste[j + 1]:
                 liste[j], liste[j + 1] = liste[j + 1], liste[j]
     return liste
-
-
-if __name__ == "__main__":
-    print(bubble_sort([5, 3, 1, 4, 2]))
